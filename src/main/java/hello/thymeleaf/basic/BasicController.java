f970953f69a87b839d3853f008d9af2cb19b47e6
rmaWhrdl <wwscan3@gmail.com>
2023-09-24 16:57:28 +0900

    <타임리프 - 기본 기능> 조건부 평가
    - if, unless
       > 타임리프는 해당 조건이 맞지 않으면 태그 자체를 렌더링하지 않는다.
       > 만약 다음 조건이 false 인 경우 <span>...<span> 부분 자체가 렌더링 되지 않고 사라진다.
          <span th:text="'미성년자'" th:if="${user.age lt 20}"></span>
    
    - switch
       > *은 만족하는 조건이 없을 때 사용하는 디폴트이다

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -120,6 +120,12 @@ public class BasicController {
         model.addAttribute("users", users);
     }
 
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
+
     @Data
     static class User {
 
